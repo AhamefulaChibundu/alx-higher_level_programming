05df25a3661d3d8850fb8627abb5d2781344291d
ahamefulachibundu <ahamefulachibundu@gmail.com>
2023-12-19 17:07:03 -0800

    A private instance of size attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Defines a square based on 0-square.py
+"""
+
+
+class Square:
+    """
+    Define a Square class
+    Attributes:
+        None
+    """
+    def __init__(self, size):
+        """
+        Initialises the attributes of Square
+        Attributes:
+            size (int): The size of a square
+        """
+        self.__size = size
