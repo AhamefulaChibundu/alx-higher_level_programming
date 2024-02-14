3b12ed75010495befbca2042853fde4d16c3945b
ahamefulachibundu <ahamefulachibundu@gmail.com>
2024-02-14 06:58:30 -0800

    creates a table and rows with different values

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,17 @@
+-- A script that creates a table second_table in the database hbtn_0c_0 in my MySQL server and add multiples rows.
+
+-- Does not throw an error if table already exist
+-- Insert values for the table
+
+CREATE TABLE IF NOT EXISTS second_table (
+	id INT,
+	name VARCHAR(256),
+	score int
+);
+
+INSERT INTO second_table (id, name, score)
+VALUES
+	(1, 'John', 10),
+	(2, 'Alex', 3),
+	(3, 'Bob', 14),
+	(4, 'George', 8);
