4d06f3d49c7b13e9dd4ee1802fb9e78d9219d2ab
ahamefulachibundu <ahamefulachibundu@gmail.com>
2024-02-15 03:29:28 -0800

    setting score without id

---
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -0,0 +1,4 @@
+-- A script that updates the score of Bob to 10 in the table second_table.
+-- without the use of Bob’s id value, only the name field
+
+UPDATE second_table SET score=10 WHERE name="Bob";
