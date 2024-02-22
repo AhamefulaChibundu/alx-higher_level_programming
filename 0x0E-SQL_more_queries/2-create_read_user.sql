4d4f774007acb97fdc7c4ca9045d4f3034982e73
ahamefulachibundu <ahamefulachibundu@gmail.com>
2024-02-22 06:46:21 -0800

    creates a database, user and gives privileges

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,8 @@
+-- creates a database hbtn_0d_2 and the user user_0d_2
+-- creates a new database
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+-- creates a new user
+CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
+-- grants priviledges to the user
+GRANT SELECT ON hbtn_0d_2.*
+TO user_0d_2@localhost;
