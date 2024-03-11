2a702b4ffe6b3acb73b1f5f4a8ed11064e8d2d28
ahamefulachibundu <ahamefulachibundu@gmail.com>
2024-03-11 07:28:16 -0700

    printing 3 lines of string with console.log

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 let string1 = 'C is fun';
 let string2 = 'Python is cool';
-let string3 = 'Javascript is amazing';
+let string3 = 'JavaScript is amazing';
 console.log(string1);
 console.log(string2);
 console.log(string3);
