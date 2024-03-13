9f5541dfc2fcad4a6c8c9ce82d7fdab1723f27de
ahamefulachibundu <ahamefulachibundu@gmail.com>
2024-03-13 15:23:08 -0700

    A function that returns th number of a particular element

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 exports.nbOccurences = function (list, searchElement) {
-    let counter = 0;
-    list.forEach(element => {
-        if (element === searchElement) {
-            counter++;
-        }
-    });
-    return counter;
+  let counter = 0;
+  list.forEach(element => {
+    if (element === searchElement) {
+      counter++;
+    }
+  });
+  return counter;
 };
